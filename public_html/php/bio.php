3cc3582800f53467fc96076a5e83169d9d5af1aa
Louis Gill <lgill7@cnm.edu>
2015-11-17 19:40:34 -0700

    added bio info

--- public_html/php/bio.php
+++ public_html/php/bio.php
@@ -5,6 +5,10 @@
 		<title>Biography</title>
 	</head>
 	<body>
-
+		<p>Dr. David Gill grew up in Niles, IL. His father died at age 37, so David went to work washing dishes and busing tables in a restaurant at age 13. He worked his way through both college and medical school in Champaign-Urbana, stocking shelves in a department store and mopping the floors of a laundromat. He completed residency training in Family Practice in Iowa, and after several years as a family physician, he switched over to full-time practice in Emergency Medicine in 1996. He is Board-certified in both Family Practice and Emergency Medicine.</p>
+		<p>Dr. Gill is a 20-year member of the Physicians for a National Health Program, an organization of 20,000 doctors which advocates for an Improved Medicare for All program. He has done volunteer work with Big Brothers/Big Sisters, The Salvation Army, and Peace Meals. He is a longtime member of Physicians for Reproductive Health.</p>
+		<p>David ran unsuccessfully as a Democrat for a seat in Congress in Illinois' old 15th District in 2004, 2006, and 2010. As a populist committed to the well-being of his fellow men and women, he always ran better than expected in that heavily Republican district. After defeating the Democratic establishment in the 2012 primary, despite being outspent 5-to-1, David barely lost the general election in the second-closest of the country's 435 House elections that year.</p>
+		<p>Dr. Gill refuses all funding from corporations and Wall Street banks.</p>
+		<p>David's first wife passed away several years ago, from colon cancer. He and his second wife, Elaine, reside in Bloomington; they have six children, two dogs rescued from animal control, and a cat.</p>
 	</body>
 </html>
