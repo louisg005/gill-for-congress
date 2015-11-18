f3ba58dc92f4e90e146c6ef8328a12d7f9e7f518
Carlos Beraun <cberaun2@cnm.edu>
2015-11-17 21:05:31 -0700

    added template from boot strap to (index.php)

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -95,7 +95,7 @@
 			</ol>
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img class="first-slide" src="../img/g4chomesamp002.jpg" alt="First slide">
+					<img class="first-slide" src="../img/g4chomesamp001.jpg" alt="First slide">
 					<div class="container">
 						<div class="carousel-caption">
 							<h1>Example headline.</h1>
@@ -105,7 +105,7 @@
 					</div>
 				</div>
 				<div class="item">
-					<img class="second-slide" src="../img/g4chomesamp002.jpg" alt="Second slide">
+					<img class="second-slide" src="../img/g4chomeslide002.jpg" alt="Second slide">
 					<div class="container">
 						<div class="carousel-caption">
 							<h1>Another example headline.</h1>
@@ -115,7 +115,7 @@
 					</div>
 				</div>
 				<div class="item">
-					<img class="third-slide" src="../img/g4chomesamp003.jpg" alt="Third slide">
+					<img class="third-slide" src="../img/g4chomeslide003.jpg" alt="Third slide">
 					<div class="container">
 						<div class="carousel-caption">
 							<h1>One more for good measure.</h1>
