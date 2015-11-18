69a351bf3552f64bf191c700abb70cd9a45e3ec3
Louis Gill <lgill7@cnm.edu>
2015-11-17 20:21:22 -0700

    added all issues info

--- public_html/php/issues.php
+++ public_html/php/issues.php
@@ -23,6 +23,24 @@
 		<h3>(Marriage Equality) It's About Time</h3>
 		<p>As a longtime champion of marriage equality, Dr. Gill was very pleased with the recent Supreme Court decision which finally corrected the stubborn and unconstitutional position of politicians such as Rodney Davis.</p>
 
-		<h3></h3>
+		<h3>(Jobs)Mission Impossible with Politicians Owned by Wall Street</h3>
+		<p>As with all other issues, economic growth and job development have been destroyed by politicians who take big contributions from Corporate America and then do the bidding of their corporate masters.  NAFTA did exactly as Ross Perot predicted back in 1992, sucking millions of good jobs out of America.  Now the Trans-Pacific Partnership is poised to do worse.</p>
+
+		<h3>(Health Care)Improved Medicare for All</h3>
+		<p>Having worked on the front lines of health care for nearly 30 years, Dr. Gill has a far better understanding of health care than most politicians.  As a longtime member of Physicians for a National Health Program, he has long argued against the dominance of insurance companies within our health care system.  Allowing insurance companies to run roughshod over American citizens leaves us far less healthy and far less wealthy.  Allowing insurance companies to draft the Affordable Care Act was yet another dereliction of duty by politicians who take the big contributions from such companies and then turn their backs on their constituents.</p>
+
+		<h3>(Foreign Policy)Experts Bought and Sold</h3>
+		<p>(Irresponsible foreign policy driven by arms manufacturers)As with all policy, our foreign policy has been driven by the corporate ownership of both the Democrats and the Republicans.  During his first campaign for Congress, Dr. Gill argued vociferously against the impending invasion of Iraq in 2003.  Time has shown him to have been right, as that invasion made America and the world less safe; it did enrich the treasure of a small few, though.</p>
+
+		<h3>(Climate)Climate Change Is Real</h3>
+		<p>(No Issue More Important)In the battle to leave behind an Earth which might be habitable for our descendents, we are stymied by politicians of both major parties who take large checks from Big Oil companies, and then profess to question the certain validity of climate change.  Our children in 20-40 years will be demanding answers, wondering how we could possibly have cast votes for such deniers of clear-cut scientific fact.</p>
+
+		<h3>(Campaign Finance)Both Major Parties Work for Their Corporate Masters</h3>
+		<p>(Democrats Little Different than Republicans)Even after the tremendous success of Dr. Gill's 2012 campaign, the Democratic establishment refused to support further effort's by him to secure Illinois' 13th District seat in the U.S. House. No surprise, given that Gill had long criticized the corporate ownership of the Democratic establishment.  In bowing down before the Wall Street banks and the large multi-national corporations, the Democrats have demonstrated the same lack of concerns for ordinary Americans as that evidenced by the Republican establishment for many decades.</p>
+
+		<h3>Why Independent?</h3>
+		<p>My staff and I are very excited about my 2016 Congressional campaign, and I'm especially proud to be running as an Independent.  I have long criticized both major parties for their financing by Wall Street interests, large corporations, and the ultra-wealthy.  The policies and politics of both the Democrats and the Republicans reflect this financing.  Neither party serves the well-being of ordinary Americans-- we have been abandoned.</p>
+		<p>Americans recognize this betrayal, and their frustration mounts.  My criticism of our corrupted politics allowed me to upset the Democratic establishment candidate in the 2012 primary, and was crucial in my near-upset victory in the Republican-leaning IL-13 in the 2012 general election.  Our polling demonstrates that my Independent campaign will be highly competitive, in spite of the obstacles built into the system by the two major parties.</p>
+		<p>Within what amounts to a one-party system, most American citizens have been without a true voice in Washington for far too long.  We deserve much better.  I look forward to discussing the issues, securing a place on the ballot, and, ultimately, to winning a majority of the votes cast in IL-13 in November 2016.</p>
 	</body>
 </html>
