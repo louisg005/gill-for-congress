baa2d2fad193ce5488f793f6f0e8d70d8a0f587e
Carlos Beraun <cberaun2@cnm.edu>
2015-11-17 18:56:01 -0700

    added template from boot strap to (index.php)

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -81,7 +81,7 @@
 			</ol>
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img class="first-slide" src="http://www.greatnewsradio.org/wp-content/uploads/2010/09/Singing_Men_Clinton_Sept252.jpg" alt="First slide">
+					<img class="first-slide" src="" alt="First slide">
 					<div class="container">
 						<div class="carousel-caption">
 							<h1>Example headline.</h1>
