6d40783bdd5415d728e89df90e89b4953738066c
Carlos Beraun <cberaun2@cnm.edu>
2015-11-17 18:21:35 -0700

    added template from boot strap to (index.php)

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -13,6 +13,9 @@
 
 		<title>Carousel Template for Bootstrap</title>
 
+		<!-- Bootstrap core CSS -->
+		<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
