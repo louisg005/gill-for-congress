251e2ffffea398f6f1a85d17a93daf6c128c8da6
Louis Gill <lgill7@cnm.edu>
2015-11-17 20:06:41 -0700

    started issues info

--- public_html/php/issues.php
+++ public_html/php/issues.php
@@ -5,6 +5,24 @@
 		<title>Issues</title>
 	</head>
 	<body>
+		<h3>(Veterans)Politicians Use Veterans as Tools</h3>
+		<p>Make veterans a true priority rather than another talking point. Career politicians of both parties recognize that they can score points with voters by expressing support for vets. But this is just cynical maneuvering when those same politicians put the desires of Wall Street ahead of the needs of veterans. Wall Street and large corporations fund the pols, and the pols make them their top priority.</p>
 
+		<h3>(Trade)"Free" Trade Crushes Most Americans</h3>
+		<p>First NAFTA, now the TPP-- politicians of both major parties work for their corporate masters. After almost 25 years, there is no doubt that such trade agreements send our jobs overseas. And yet, here we go again with the Trans-Pacific Partnership-- this one stinks so bad that they've had to keep the details of the agreement a secret from us. And not only do such deals bring great economic harm-- they are written in a way that ultimately costs us our sovereignty as a nation. Our laws become meaningless when we allow trade agreements to circumvent and supercede our laws.</p>
+
+		<h3>(Taxes)Taxed Enough Already</h3>
+		<p>When we no longer have representatives owned by oil companies, arms manufacturers, insurance and drug companies, and Wall Street banks, we can have an economy and tax system that works to the benefit of ordinary Americans. As long as we continue to elect Republicans and Democrats who serve their corporate masters, we will continue to be over-taxed and under-served.</p>
+
+		<h3>(Reproductive Health)Keep the Government Out</h3>
+		<p>As a physician, I've provided care to thousands of pregnant women. Politicians have no idea of the details and the complexity of the situations presented into the lives of these women. Accordingly, representatives in Congress should stay out of such business, and leave the decision-making to the pregnant women themselves.</p>
+
+		<h3>(Politics)TIME FOR TERM LIMITS</h3>
+		<p>Both parties want to maintain their positions of royalty. Dr. Gill has long argued for term limits on U.S. Representatives and Senators, as a means of working toward restoring our democracy. He has long questioned both major political parties, and as he watched the national Democrats abandon the citizens of IL-13 after the general election of 2012, it became painfully clear to Gill that there will be no improvement in Americans' lives until we abandon both parties and install true independents in positions of leadership.</p>
+
+		<h3>(Marriage Equality) It's About Time</h3>
+		<p>As a longtime champion of marriage equality, Dr. Gill was very pleased with the recent Supreme Court decision which finally corrected the stubborn and unconstitutional position of politicians such as Rodney Davis.</p>
+
+		<h3></h3>
 	</body>
 </html>
