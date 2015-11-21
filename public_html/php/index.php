f724db47fe7f87a3739ae8426a4f2bfa3aa227f1
Louis Gill <lgill7@cnm.edu>
2015-11-20 23:56:34 -0700

    fixed image

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -46,7 +46,6 @@
 
 			<!-- Image -->
 			<div class="container">
-				<div class="row">
 					<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
 						<img class="pull-right" src="../img/g4chomeslide004.jpg" alt="David Gill"/>
 					</div>
