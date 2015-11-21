cab709fbfdc4e03f8367693ac502266614a40946
Louis Gill <lgill7@cnm.edu>
2015-11-20 23:49:31 -0700

    added image

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -44,6 +44,15 @@
 				</ul>
 			</div>
 
+			<!-- Image -->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
+						<img class="pull-right src="../img/g4chomeslide004.jpg" alt="David Gill"/>
+					</div>
+				</div>
+			</div>
+
 	<!-- Horizontal Buttons on Home Page -->
 	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 		<div class="row placeholders">
