0cfed5b764b4c7ebd433e17d5154764c08d1aa14
Louis Gill <lgill7@cnm.edu>
2015-11-21 00:06:22 -0700

    fixed image

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -47,9 +47,8 @@
 			</div>
 
 			<!-- Image -->
-			<div class="container">
+			<div class="container-bg">
 				<div class="row">
-					<div class="container-bg">
 					<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
 					</div>
 					</div>
