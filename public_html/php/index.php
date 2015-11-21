3b8933a3ea34325ff7554b2b4e4b9ae8b65b715c
Louis Gill <lgill7@cnm.edu>
2015-11-20 23:23:48 -0700

    added doc block for sidebar

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -43,11 +43,6 @@
 					<li><a href="#">Donate</a></li>
 				</ul>
 			</div>
-		</div>
-	</div>
-
-
-
 
 	<!-- Horizontal Buttons on Home Page -->
 	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
