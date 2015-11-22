79a8f6f557b07e00e23725708794f040a04a7ae8
Carlos Beraun <cberaun2@cnm.edu>
2015-11-21 21:10:34 -0700

    added bootstrap template to page ---->(index002.php)

--- public_html/php/index002.php
+++ public_html/php/index002.php
@@ -33,31 +33,11 @@
 
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-1">
+				<div class="col-md-6">
 					Container Left
-					<ul class="nav nav-sidebar">
-						<li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-						<li><a href="#">Reports</a></li>
-						<li><a href="#">Analytics</a></li>
-						<li><a href="#">Export</a></li>
-					</ul>
-					<ul class="nav nav-sidebar">
-						<li><a href="">Nav item</a></li>
-						<li><a href="">Nav item again</a></li>
-						<li><a href="">One more nav</a></li>
-						<li><a href="">Another nav item</a></li>
-						<li><a href="">More navigation</a></li>
-					</ul>
-					<ul class="nav nav-sidebar">
-						<li><a href="">Nav item again</a></li>
-						<li><a href="">One more nav</a></li>
-						<li><a href="">Another nav item</a></li>
-					</ul>
-				</div>
-				</div>
 
-
-				<div class="col-md-11">
+				</div>
+				<div class="col-md-6">
 					Container Right
 				</div>
 			</div>
